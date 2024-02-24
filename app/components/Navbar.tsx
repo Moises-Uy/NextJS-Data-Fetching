37047e854956d0a2b88b283f875b046ef6d44263
Moises Uy <it.moisesuy@gmail.com>
2024-02-24 09:46:57 +0800

    This

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,14 @@
+export default function Navbar() {
+  return (
+    <>
+      <nav>
+        <h1>Hello</h1>
+        <ul>
+          <li>About</li>
+          <li>About</li>
+          <li>About</li>
+        </ul>
+      </nav>
+    </>
+  );
+}
